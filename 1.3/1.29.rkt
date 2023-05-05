dd807bb1c14dd63d062f9cbdfd875a52dddd5408
максим <ivankoklyaev@gmail.com>
2023-05-05 20:03:13 +0300

    add 1.29

--- 1.3/1.29.rkt
+++ 1.3/1.29.rkt
@@ -1,21 +1,25 @@
 #lang racket
-  
- (define (simpson-integral f a b n) 
-   (define (sum term a next b) 
-     (if (> a b) 
-       0 
-       (+ (term a) (sum term (next a) next b)))) 
-   (define (term x) 
-     (+ (f x) (* 4 (f (+ x h))) (f (+ x (* 2 h))))) 
-   (define (next x) 
-     (+ x (* 2 h))) 
-   (define h (/ (- b a) n)) 
-   (* (/ h 3) (sum term a next (- b (* 2 h))))) 
-  
-  
- (define (cube x) 
-   (* x x x)) 
-  
-  
- (display (simpson-integral cube 0 1 100)) (newline) 
- (display (simpson-integral cube 0 1 1000)) (newline) 
+
+;; we had to modify the condition because in other way 
+;; we had wrong answer or ugly kind of argument
+(define (sum term a next b)
+    (if (or (> a b) (= a b)) 
+        0 
+        (+ (term a) (sum term (next a) next b))))
+
+;; get-triple is correct because n is always an even integer
+(define (simpson-integral f a b n) 
+    (define h (/ (- b a) n)) 
+    (define (get-triple x)
+        (+ (f x) 
+           (* 4 (f (+ x h))) 
+           (f (+ x (* 2 h)))))
+    (define (step x)
+        (+ x (* 2 h)))
+    (* (/ h 3) (sum get-triple a step b)))
+    
+(define (cube x) 
+    (* x x x)) 
+
+(simpson-integral cube 0 1 100)
+(simpson-integral cube 0 1 1000)
